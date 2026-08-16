ecf6b6fde3d5205b6fa2be4c34f6028fc5abe169
jhryytheise <50068838+jhryytheise@users.noreply.github.com>
2026-08-16 16:51:10 +0800

    Update Footer.vue

--- assets/Footer.vue
+++ assets/Footer.vue
@@ -20,9 +20,9 @@
     </div>
 
     <div class="project-links">
-      <strong>项目:</strong><a href="https://github.com/willow-god/FlareDrive-R2" target="_blank"
+      <strong>项目:</strong><a href="https://github.com/jhryytheise/FlareDrive-R2" target="_blank"
         rel="noopener noreferrer">FlareDrive-R2</a>
-      <strong>作者:</strong><a href="https://github.com/willow-god" target="_blank"
+      <strong>作者:</strong><a href="https://github.com/jhryytheise" target="_blank"
         rel="noopener noreferrer">LiuShen</a>
       <strong>初始项目:</strong><a href="https://github.com/ljxi/Cloudflare-R2-oss" target="_blank"
         rel="noopener noreferrer">Cloudflare-R2-OSS</a>
@@ -31,7 +31,7 @@
     <div class="powered">Powered by <a href="https://workers.cloudflare.com/" target="_blank"
         rel="noopener noreferrer">Cloudflare Workers</a></div>
 
-    <div style="margin:10px 0;">© 2025 FlareDrive. All rights reserved.</div>
+    <div style="margin:10px 0;">© 2026 FlareDrive. All rights reserved.</div>
   </footer>
 </template>
 
@@ -40,10 +40,10 @@ export default {
   name: "Footer",
   data() {
     return {
-      homeUrl: "https://www.liushen.fun/",
-      blogUrl: "https://blog.liushen.fun/",
-      githubUrl: "https://github.com/willow-god",
-      emailUrl: "mailto:01@liushen.fun"
+      homeUrl: "https://vpn.gaijiao.fun/",
+      blogUrl: "https://gaijiao.fun/",
+      githubUrl: "https://github.com/jhryytheise",
+      emailUrl: "j@gaijiao.fun"
     };
   }
 };
